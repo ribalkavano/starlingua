0d575e0933d828121f4b8893609d231f4dcdcdc4
Ivan Rybalka <rybalkavano@gmail.com>
2016-04-01 13:57:03 +0300

    Gulp assembly

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+var gulp         = require('gulp'),
+	 sass         = require('gulp-sass'),
+	 browserSync  = require('browser-sync'),
+	 concat       = require('gulp-concat'),
+	 uglify       = require('gulp-uglifyjs'),
+	 cssnano      = require('gulp-cssnano'),
+	 rename       = require('gulp-rename'),
+	 del          = require('del'),
+	 imagemin     = require('gulp-imagemin'),
+	 pngquant     = require('imagemin-pngquant'),
+	 cache        = require('gulp-cache'),
+	 autoprefixer = require('gulp-autoprefixer');
+
+
+// =========================
+//          TASKS
+// =========================
+
+gulp.task('sass', function() {
+	return gulp.src('app/sass/**/*.sass')
+	.pipe(sass())
+	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+		])
+	.pipe(concat('libs.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js'));
+});
+
+gulp.task('css-libs', ['sass'], function() {
+	return gulp.src('app/css/libs.css')
+	.pipe(cssnano())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task('clean', function() {
+	return del.sync('dist');
+});
+
+// Clear cache
+gulp.task('clear', function() {
+	return cache.clearAll();
+});
+
+// Minimize img size
+gulp.task('img', function() {
+	return gulp.src('app/img/**/*')
+	.pipe(cache(imagemin({
+		interlaced: true,
+		progressive: true,
+		svgoPlugins: [{removeViewBox: false}],
+		use: [pngquant()]
+	})))
+	.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
+	gulp.watch('app/sass/**/*.sass', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+// Prodaction
+gulp.task('build', ['clean', 'img', 'sass', 'scripts'], function() {
+
+	var buildCss = gulp.src('app/css/**/*')
+		.pipe(gulp.dest('dist/css'));
+
+	var buildJs = gulp.src('app/js/**/*')
+		.pipe(gulp.dest('dist/js'));
+
+	var buildFonts = gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+
+	var buildHtml = gulp.src('app/*.html')
+		.pipe(gulp.dest('dist'));
+
+});
+
+
+
+
+
+
+
+
